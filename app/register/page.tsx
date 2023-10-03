3ad38fb13b017f4533d77dc0eabf963453ae9c07
Snehal Opash <sp.opash@gmail.com>
2023-10-03 16:09:38 +0530

    fix cors

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -43,11 +43,10 @@ const Register = () => {
         `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/signup`
       );
       const response = await axios.post(
-        // "http://localhost:3000/api/auth/signup",
+        "http://localhost:3000/api/auth/signup",
         // `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/signup`,
-        `https://trello-clone-ruddy-gamma.vercel.app/api/auth/signup`,
-        options.headers,
-        values
+        // `https://trello-clone-ruddy-gamma.vercel.app/api/auth/signup`,
+        values,
       );
 
       console.log(">>>>>>>>>>>>>", response);
