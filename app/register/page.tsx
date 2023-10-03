e084d121a0878df03579e87d8662aa9cf0c28c3b
Snehal Opash <sp.opash@gmail.com>
2023-10-03 10:50:35 +0530

    fix 3

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -40,8 +40,8 @@ const Register = () => {
       );
       const response = await fetch(
         // "http://localhost:3000/api/auth/signup",
-        `/app/api/auth/signup`,
-        //  `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/signup`,
+        // `/api/auth/signup`,
+         `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/signup`,
         options
       );
       if (response.ok) {
